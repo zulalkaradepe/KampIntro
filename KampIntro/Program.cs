7e8564de74dad56c75263fa1861eb810b63ccf7a
zulalkaradepe <zulalkaradepe12@gmail.com>
2024-04-17 16:41:00 +0300

    Proje dosyası ekle.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmısMı = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if( dolarDun< dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+
+            if (sistemeGirisYapmısMı==true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
---
+++ Metotlar/Urun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class Urun
+    {
+        //property - özellik
+        public int Id { get; set; }
+        public int Fiyati { get; set; }
+        public string Adi { get; set; }
+        public string Acıklama { get; set; }
+        public int StokAdedi { get; set; }
+    }
+}
