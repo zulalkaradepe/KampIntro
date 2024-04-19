405af09b6d0af1295dd80902913d523f878ccf43
zulalkaradepe <zulalkaradepe12@gmail.com>
2024-04-19 15:16:02 +0300

    23 ocak çalışması

---
+++ OOP3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+           
+            IKrediManager tasıtKrediManager = new TasıtKrediManager();
+            
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService>() {new SmsLoggerService(), new FileLoggerService() };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKrediManager(), loggers);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasıtKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
